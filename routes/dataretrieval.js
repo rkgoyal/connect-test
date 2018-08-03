aa2e4eeeb5041d27a6062cfd965262d996610d54
Ravi <ravigoyal@Ravis-MacBook-Pro.local>
2018-08-03 09:45:09 -0400

    Get sources route and controller now are specific to the logged in user

--- routes/dataretrieval.js
+++ routes/dataretrieval.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// Require our controllers.
+var dataretrieval_controller = require('../controllers/dataRetrieval');
+
 // GET dashboard page
 router.get('/', function(req, res) {
   if (req.user) {
@@ -9,5 +12,10 @@ router.get('/', function(req, res) {
     res.redirect('/auth/login');
   }
 });
+
+// GET sources associated with the User
+router.get('/sources', dataretrieval_controller.get_sources);
+
+
 
 module.exports = router;
