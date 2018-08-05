683ba5b035400ecb6e6906bc4031c13c7fd28337
Ravi <ravigoyal@Ravis-MacBook-Pro.local>
2018-08-04 23:57:38 -0400

    Added bootstrap; begun clean up of all pages

--- routes/profile.js
+++ routes/profile.js
@@ -37,21 +37,22 @@ router.post('/connect/finish', auth_controller.post_session_token);
 // User data in Json format
 router.get('/userdata', function(req, res) {
 
-            if (req.user === undefined) {
-                // The user is not logged in
-                res.json({});
-            } else {
-                res.json({
-                    userEmail: req.user.email,
-                    publicToken: req.user.humanapi.publicToken
-                });
-            }
+      if (req.user === undefined) {
+          // The user is not logged in
+          res.json({});
+      } else {
+          res.json({
+              userEmail: req.user.email,
+              publicToken: req.user.humanapi.publicToken
+          });
+      }
 });
 
 // Render the dashboard page after data source connections
 router.get('/dashboard', function(req, res) {
   if (req.user) {
-    res.render('dashboard', {title: 'My Health Dashboard'});
+    var name = req.user.name;
+    res.render('dashboard', {title: 'My Health Dashboard', user: name});
   } else {
     res.redirect('/auth/login');
   }
