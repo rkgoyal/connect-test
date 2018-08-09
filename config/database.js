2442dca6c8d284f0472602cbfc67ec342fffeb47
Ravi <ravigoyal@Ravis-MacBook-Pro.local>
2018-08-09 17:58:09 -0400

    Added gitignore

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
   database:'mongodb://localhost:27017/connect-test',
+  database:'mongodb://testappv1:Lty2Mpve5FX@ds213832.mlab.com:13832/alfredhealthv0',
   secret: 'trendhealth'
 }
